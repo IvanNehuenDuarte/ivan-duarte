e1eaf2ea4c97096df0aab19c954adadc1aa314c5
Ivan Nehuen Duarte <ivan.nehuen.duarte@gmail.com>
2024-11-04 20:08:52 -0300

    About

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 /*
  * Components
  */
+import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -10,6 +11,7 @@ const App = () => {
       <Header />
       <main>
         <Hero />
+        <About />
       </main>
     </>
   );
