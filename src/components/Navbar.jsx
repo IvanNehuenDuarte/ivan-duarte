97724e0ce48e5d17fd65363bf7f95407f3769cfd
Ivan Nehuen Duarte <ivan.nehuen.duarte@gmail.com>
2024-11-05 15:25:42 -0300

    Arreglo de error en consola "style undefined" en activebox

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,14 +9,24 @@ const Navbar = ({ navOpen }) => {
   const activeBox = useRef();
 
   const initActiveBox = () => {
-    activeBox.current.style.top = lastActiveLink.current.offsetTop + "px";
-    activeBox.current.style.left = lastActiveLink.current.offsetLeft + "px";
-    activeBox.current.style.width = lastActiveLink.current.offsetWidth + "px";
-    activeBox.current.style.height = lastActiveLink.current.offsetHeight + "px";
+    if (activeBox.current && lastActiveLink.current) {
+      activeBox.current.style.top = lastActiveLink.current.offsetTop + "px";
+      activeBox.current.style.left = lastActiveLink.current.offsetLeft + "px";
+      activeBox.current.style.width = lastActiveLink.current.offsetWidth + "px";
+      activeBox.current.style.height =
+        lastActiveLink.current.offsetHeight + "px";
+    }
   };
 
-  useEffect(initActiveBox, []);
-  window.addEventListener("resize", initActiveBox);
+  useEffect(() => {
+    initActiveBox();
+
+    window.addEventListener("resize", initActiveBox);
+
+    return () => {
+      window.removeEventListener("resize", initActiveBox);
+    };
+  }, []);
 
   const activeCurrentLink = (event) => {
     lastActiveLink.current?.classList.remove("active");
