7e9e1dac6ed2e6e85fd703ac70e7411e47a77924
Ivan Nehuen Duarte <ivan.nehuen.duarte@gmail.com>
2024-11-05 00:56:25 -0300

    reviews

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 /*
  * Components
  */
-import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import About from "./components/About";
 import Skill from "./components/Skill";
 import Work from "./components/Work";
+import Review from "./components/Review";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <About />
         <Skill />
         <Work />
+        <Review />
       </main>
     </>
   );
