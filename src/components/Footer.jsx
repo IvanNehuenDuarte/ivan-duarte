e3efd2cb1d65192cab328a78eaeaac8ae842b414
Ivan Nehuen Duarte <ivan.nehuen.duarte@gmail.com>
2024-11-07 00:15:51 -0300

    gsap scroll

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -55,7 +55,7 @@ const Footer = () => {
       <div className="container">
         <div className="lg:grid lg:grid-cols-2">
           <div className="mb-10">
-            <h2 className="headline-1 mb-8 lg:max-w-[12ch]">
+            <h2 className="headline-1 mb-8 lg:max-w-[12ch] reveal-up">
               Let&apos;s work together today!
             </h2>
 
@@ -63,19 +63,20 @@ const Footer = () => {
               href="mailto:ivan.nehuen.duarte@gmail.com"
               label="Start project"
               icon="chevron_right"
+              classes="reveal-up"
             />
           </div>
 
           <div className="grid grid-cols-2 gap-4 lg:pl-20">
             <div>
-              <p className="mb-2">Sitemap</p>
+              <p className="mb-2 reveal-up">Sitemap</p>
 
               <ul>
                 {sitemap.map(({ label, href }, key) => (
                   <li key={key}>
                     <a
                       href={href}
-                      className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200"
+                      className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up"
                     >
                       {label}
                     </a>
@@ -85,7 +86,7 @@ const Footer = () => {
             </div>
 
             <div>
-              <p className="mb-2">Socials</p>
+              <p className="mb-2 reveal-up">Socials</p>
 
               <ul>
                 {socials.map(({ label, href }, key) => (
@@ -93,7 +94,7 @@ const Footer = () => {
                     <a
                       href={href}
                       target="_blank"
-                      className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200"
+                      className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up"
                     >
                       {label}
                     </a>
