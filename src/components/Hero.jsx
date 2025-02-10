cd8f218928594272488a5df211ca98c008baded8
Ivan Nehuen Duarte <ivan.nehuen.duarte@gmail.com>
2025-02-10 18:49:14 -0300

    change avatar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
           <div className="flex items-center gap-3">
             <figure className="img-box w-9 h-9 rounded-lg">
               <img
-                src="/images/avatar-1.jpg"
+                src="/images/avatar1.png"
                 width={40}
                 height={40}
                 alt="Iván Duarte portrait"
@@ -61,7 +61,7 @@ const Hero = () => {
         <div className="hidden lg:block">
           <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
             <img
-              src="/images/hero-banner.png"
+              src="/images/hero-ivan-avatar.png"
               width={656}
               height={800}
               alt="Iván Duarte"
